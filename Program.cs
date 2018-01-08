628a2a15d32dc8cf1e4256dac9953dae9a8550dc
keenanmcnamara12 <keenan.mcnamara12@gmail.com>
2018-01-07 22:35:56 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotlightBackgrounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserSettings us = new UserSettings();
+            string userBackgroundDirectory = us.getBackgroundFolderPath();
+
+            // Make sure user background folder exists
+            if (String.IsNullOrWhiteSpace(userBackgroundDirectory))
+                return;
+
+            string local = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string spotlightDirectory = local + "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\";
+
+            // In case this stuff gets moved in the future
+            if (!Directory.Exists(spotlightDirectory))
+                return;
+
+            string[] spotlightFiles = Directory.GetFiles(spotlightDirectory);
+            string[] backgroundFiles = Directory.GetFiles(userBackgroundDirectory);
+
+            for (int i = 0; i < spotlightFiles.Length; i++)
+            {
+                // Only copy files from the spotlight directory that aren't already in the background directory
+                if (!(Array.IndexOf(backgroundFiles, spotlightFiles[i] + ".jpg") > -1))
+                {
+                    // Verify the file is a jpeg
+                    if (!hasJpegHeader(spotlightFiles[i]))
+                        continue;
+
+                    // Ensure dimensions are okie dokie per user specification (which actually defaults if the user doesn't specify)
+                    System.Drawing.Image img = System.Drawing.Image.FromFile(spotlightFiles[i]);
+                    if ((img.Width < us.getMinPixelWidth()) || (img.Height < us.getMinPixelHeight()))
+                        continue;
+
+                    File.Copy(spotlightFiles[i], userBackgroundDirectory + "\\" + getFileName(spotlightFiles[i]) + ".jpg", true);
+                }
+            }
+        }
+
+        // https://stackoverflow.com/questions/772388/c-sharp-how-can-i-test-a-file-is-a-jpeg
+        // Users the magic number in jpeg to verify the file type
+        static bool hasJpegHeader(string fileName)
+        {
+            using (BinaryReader br = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                UInt16 soi = br.ReadUInt16();       // Start of image (SOI) marker (FFD8)
+                UInt16 marker = br.ReadUInt16();    // JFIF marker (FFEO) or EXIF marker (FF01)
+                return soi == 0xd8ff && (marker & 0xe0ff) == 0xe0ff;
+            }
+        }
+
+        static string getFileName(string filePath)
+        {
+            string[] tmp = filePath.Split('\\');
+            return tmp[tmp.Length - 1];
+        }
+    }
+}
---
+++ UserSettings.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotlightBackgrounds
+{
+    class UserSettings
+    {
+        /// <summary>
+        /// Path to the application data file
+        /// </summary>
+        private string _path;
+
+        /// <summary>
+        /// Setting for the where the backgrounds are stored
+        /// </summary>
+        private string _backgroundFolderPath;
+
+        /// <summary>
+        /// Setting for the minimum pixel height
+        /// </summary>
+        private int _minPixelHeight;
+
+        /// <summary>
+        /// Setting for the minimum pixel height
+        /// </summary>
+        private int _minPixelWidth;
+
+
+        public UserSettings()
+        {
+            _path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SpotlightBackground\\preferences.txt";
+            if (File.Exists(_path))
+            {
+                using (StreamReader sr = new StreamReader(_path))
+                {
+                    string line = sr.ReadLine();
+                    if (line.Contains("BackgroundFolderPath="))
+                    {
+                        string[] sides = line.Split('=');
+                        string potentialDirectory = sides[1];
+                        if (Directory.Exists(potentialDirectory))
+                            _backgroundFolderPath = potentialDirectory;
+                        else
+                            _backgroundFolderPath = null;
+                    }
+
+                    line = sr.ReadLine();
+                    if (line.Contains("MinPixelHeight="))
+                    {
+                        string[] sides = line.Split('=');
+                        int pixels = Int32.Parse(sides[1]);
+                        if (pixels > 0)
+                            _minPixelHeight = pixels;
+                        else
+                            _minPixelHeight = 1920;
+                    }
+
+                    line = sr.ReadLine();
+                    if (line.Contains("MinPixelWidth="))
+                    {
+                        string[] sides = line.Split('=');
+                        int pixels = Int32.Parse(sides[1]);
+                        if (pixels > 0)
+                            _minPixelWidth = pixels;
+                        else
+                            _minPixelWidth = 1080;
+                    }
+
+                }
+            }
+            else
+            {
+                // First we'll likely need to create the directory
+                FileInfo fi = new FileInfo(_path);
+                if (!fi.Directory.Exists)
+                {
+                    Directory.CreateDirectory(fi.DirectoryName);
+                }
+
+                // create the file (this is the first time the application has been run I guess)
+                using (StreamWriter sw = File.AppendText(_path))
+                {
+                    sw.WriteLine("BackgroundFolderPath=");
+                    sw.WriteLine("MinPixelHeight=");
+                    sw.WriteLine("MinPixelWidth=");
+                }
+                _backgroundFolderPath = null;
+            }
+        }
+
+        /// <summary>
+        /// The user specified background folder path.
+        /// </summary>
+        /// <returns>A valid directory or null.</returns>
+        public string getBackgroundFolderPath()
+        {
+            return _backgroundFolderPath;
+        }
+
+        public int getMinPixelHeight()
+        {
+            return _minPixelHeight;
+        }
+
+        public int getMinPixelWidth()
+        {
+            return _minPixelWidth;
+        }
+    }
+}
